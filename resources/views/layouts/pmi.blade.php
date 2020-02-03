fe10fc9808d1790dc8e892ff7d93d228f7f0c0f9
Eghar Shafiera <52167669+egharshafiera@users.noreply.github.com>
2020-02-03 09:40:20 +0700

    pdf update 1.0

--- resources/views/layouts/pmi.blade.php
+++ resources/views/layouts/pmi.blade.php
@@ -5,40 +5,97 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">{{--
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+--}}
 </head>
 
 <body>
-<style type="text/css">
-    table tr{
-        text-align: center;
-    }
-</style>
+    <style type="text/css">
+        table tr{
+            text-align: center;
+        }
 
-<table width="100%">
-    <tr>
-        <td width="80" align="left"><img src="{{ public_path('assets/img/picture1.png') }}" > <br></td>
-    </tr>
-</table>
-<hr>
-@yield('content')
-<hr>
-<br>
-<br>
+        table.table-borderless td, .table.table-borderless th {
+            border: 0 !important;
+        }
+    </style>
 
-<table class="table table-borderless" text-align="center">
+
+    <table width="100%">
+        <tr>
+            <td width="80" align="left"><img src="{{ public_path('assets/img/picture1.png') }}" > <br></td>
+        </tr>
+    </table>
+    <hr>
+<div class="container">
+    @yield('content')
+    <br>
+            {{--<div class="row-cols-3" >
+                <div class="col">                                       </div>
+                <div class="col"> Mengetahui,                           </div>
+                <div class="col"> Menyetujui,                           </div>
+            </div>
+            <div class="row" >
+                <div class="col"> Pemohon        </div>
+                <div class="col"> Ka. Unit Kerja/Atasan Langsung        </div>
+                <div class="col"> Ka. Bidang Infrastruktur Informasi    </div>
+            </div>
+            <div class="row">
+                <div class="col"> __________________                    </div>
+                <div class="col"> __________________                    </div>
+                <div class="col"> __________________                    </div>
+            </div>
+            <div class="row" style="background-color: #2b542c">
+                <div class="col">                                       </div>
+                <div class="col">                                       </div>
+                <div class="col"> Amir Dahlan, S.T.,M. Kom              </div>
+            </div>--}}
+
+
+    <table class="table table-borderless">
+        <thead>
+        <tr >
+            <th scope="col-sm-4" ></th>
+            <th scope="col-sm-4" ></th>
+            <th scope="col-sm-4" ></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>                                       </td>
+            <td> Mengetahui,                           </td>
+            <td> Menyetujui,                           </td>
+        </tr>
+
+        </tbody>
+    </table>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+{{--<table class="table table-borderless" text-align="center">
+
     <thead>
     <tr >
-        <th scope="col">{{--created at--}}</th>
+        <th scope="col">--}}{{--created at--}}{{--</th>
         <th scope="col">Mengetahui,</th>
         <th scope="col">Menyetujui,</th>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td width="100">Pemohon</td>
-        <td width ="140" >Ka. Unit Kerja/Atasan Langsung </td>
-        <td width ="140">Ka. Bidang Infrastruktur Informasi</td>
+        <td wi--}}{{--dth="100"--}}{{-->Pemohon</td>
+        <td --}}{{--width ="140"--}}{{-- >Ka. Unit Kerja/Atasan Langsung </td>
+        <td --}}{{--width ="140"--}}{{-->Ka. Bidang Infrastruktur Informasi</td>
     </tr>
     <tr>
         <td colspan="3" height ="70">  </td>
@@ -55,8 +112,7 @@
         <td> Amir Dahlan, S.T.,M. Kom </td>
     </tr>
     </tbody>
-</table>
+</table>--}}
 
 
-</body>
-</html>
+
--- resources/views/vendor/formbuilder/my_submissions/submission_pdf.blade.php
+++ resources/views/vendor/formbuilder/my_submissions/submission_pdf.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 
+
     <center>
-        <h5>
+        <h4>
             <strong>
                 <u>{{ $submission->form->name }}</u>
                 <br>
             </strong>
-        </h5>
-        <h5>
+        </h4>
+        <h4>
             {{ $submission->form->letter_code }}
-        </h5>
+        </h4>
     </center>
 
     <style type="text/css">
@@ -22,61 +23,61 @@
 
     </style>
     <br>
-    <p><b>IDENTITAS PEMOHON</b></p>
-    <table class="table table-borderless table-sm" width="100%">
+
+
+        <p><b>IDENTITAS PEMOHON</b></p>
+
+        <table class="table table-borderless">
+          <thead>
+            <tr >
+                <th scope="col" width="50%"></th>
+                <th scope="col" width="50%"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td >Nama Lengkap</td>
+                <td >: {{ $submission->user->name  }}</td>
+            </tr>
+            <tr>
+                <td >Email </td>
+                <td >: {{ $submission->user->email }}</td>
+            </tr>
+            @foreach($submission_data as $sub)
+            <tr>
+                <td >NIP</td>
+                <td >: {{ $sub->nip }} / {{$sub->nip18}}</td>
+            </tr>
+            <tr>
+                <td >Unit Kerja</td>
+                <td >: {{ $sub->nama_unit }}</td>
+            </tr>
+            <tr>
+                <td >No HP</td>
+                <td >: {{ $sub->no_hp }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+
+        <p><b>DATA</b></p>
+        <table class="table table-borderless" >
 
         <thead>
         <tr >
-            <th scope="col"></th>
-            <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-
-        <tr>
-            <td width ="50%">Nama Lengkap</td>
-            <td width ="50%">: {{ $submission->user->name  }}</td>
-        </tr>
-        <tr>
-            <td width ="50%">Email </td>
-            <td width ="50%">: {{ $submission->user->email }}</td>
-        </tr>
-        @foreach($submission_data as $sub)
-            <tr>
-                <td width ="50%">NIP</td>
-                <td width ="50%">: {{ $sub->nip }} / {{$sub->nip18}}</td>
-            </tr>
-            <tr>
-                <td width ="50%">Unit Kerja</td>
-                <td width ="50%">: {{ $sub->nama_unit }}</td>
-            </tr>
-            <tr>
-                <td width="50%">No HP</td>
-                <td width ="50%">: {{ $sub->no_hp }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
-
-
-    <p><b>DATA</b></p>
-    <table class="table table-borderless table-sm" width="100%">
-
-        <thead>
-        <tr >
-            <th scope="col"></th>
-            <th scope="col"></th>
+            <th scope="col" width="50%"></th>
+            <th scope="col" width="50%"></th>
         </tr>
         </thead>
         <tbody>
         @foreach($form_headers as $header)
             <tr>
-                <td width="50%">{{ $header['label'] ?? title_case($header['name']) }} </td>
-                <td width ="50%">: {{ $submission->renderEntryContent($header['name'], $header['type']) }}</td>
+                <td>{{ $header['label'] ?? title_case($header['name']) }} </td>
+                <td >: {{ $submission->renderEntryContent($header['name'], $header['type']) }}</td>
             </tr>
         @endforeach
         </tbody>
     </table>
+
 
 @endsection
