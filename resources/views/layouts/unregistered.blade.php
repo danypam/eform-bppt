ffc7744a54850e310f766bf80f1aa20d494c2cd8
Fajaragst <Fajaragst@users.noreply.github.com>
2020-03-03 12:26:30 +0700

    Update unregistered.blade.php

--- resources/views/layouts/unregistered.blade.php
+++ resources/views/layouts/unregistered.blade.php
@@ -3,15 +3,17 @@
         <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
     </head>
     <body>
+    <div class="container">
         <div class="jumbotron">
             <h1 class="display-4">Gagal Login</h1>
             <p class="lead">Akun anda belum terdaftar dalam sistem</p>
             <hr class="my-4">
             <p>Kontak Administrator</p>
             <p class="lead">
-                <a class="btn btn-primary btn-lg" href="{{route('cas.logout')}}" role="button">Log Out</a>
+                <a class="btn btn-primary btn-lg" href="/logout" role="button">Log Out</a>
             </p>
         </div>
+    </div>
     </body>
 </html>
 
