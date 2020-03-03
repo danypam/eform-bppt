7110337da1a017116094feb327c4dfb0f16d10cc
Eghar Shafiera <52167669+egharshafiera@users.noreply.github.com>
2020-03-03 14:54:05 +0700

    tanpa cas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
     {
 //        LogActivity::addToLog('User Was Logout');
         Auth::logout();
-        Cas::logout();
+        //Cas::logout();
 
         return redirect('/login');
     }
