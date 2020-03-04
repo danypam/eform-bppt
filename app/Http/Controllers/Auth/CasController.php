944d3f326591f6cb34843ed1805414e998d8c61c
alamwahyu <52119780+alamwahyu@users.noreply.github.com>
2020-03-04 14:32:40 +0700

    3

--- app/Http/Controllers/Auth/CasController.php
+++ app/Http/Controllers/Auth/CasController.php
@@ -15,6 +15,7 @@ class CasController extends Controller
     {
         try {
             $id = '';
+            dd(Cas::attributes['Email']);
             foreach (Cas::user()->attributes['Email'] as $email){
                 if (fnmatch("*bppt.go.id",$email)){
                     $id = $email;
