3b9a6467e13e8f672287cce38ec969a612513df5
Fajaragst <Fajaragst@users.noreply.github.com>
2020-03-12 11:19:50 +0700

    fix final

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,8 +38,8 @@ class AuthController extends Controller
     {
         LogActivity::addToLog('User Was Logout');
         Auth::logout();
-        //Cas::logout();
-        return redirect('/login');
+        cas()->logout('', url('/'));
+        //return redirect('/login');
     }
 
     public function edit(Request $request)
